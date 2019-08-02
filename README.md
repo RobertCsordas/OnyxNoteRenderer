3390fb09eda03ce3915cb0e4e6e85f2ebc86ca2a
Róbert Csordás <xdever@gmail.com>
2019-08-02 12:10:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Onyx Boox note renderer
+
+Renders PDF files from Onyx Boox Note app backup zip file.
+
+Tested with Onyx Boox Note Pro, Firmware 2.1.2.
+
+## What can it do?
+
+It can read the backup file (a collection of SQLite databases) and reder PDFs out of them.
+
+Currently it ignores the pressure of the strokes.
+
+The text transcription of each stroke is also available in the files, so it could be overlaid on the PDF in order to be searchable, but this is not done here.
+
+## Usage
+
+```bash
+./onyx_render.py Note.zip notes 
+```
+
+It will create a new directory *notes* with the same directory structure as in the reader's Note app and render each note as a pdf inside.
+
+It can take quite a long time, I haven't optimized for speed.
+
+## File format
+
+### ShapeDatabase.db 
+
+A file describing the names and directory structure of the notes. 
+Table NoteModel has many interesting fields:
+```
+uniqueId: the identifier of the note/directory. In case of note, this is also the filename
+parentUniqueId: the uniqueId of parent directory
+title: human readable filename
+type: 0 - dir, 1 - note    
+pageNameList: the list of page IDs in correct order. Needed to recover page order.
+``` 
+
+### UID.db
+
+These are the individual notes, where the UID matches the uniqueId from the ShapeDatabase.db. The strokes are saved in table NewShapeModel:
+
+```
+points - a binary blog of points. See later
+thickness - line thickness
+matrixValues - transformation matrix
+pageUniqueId - the page containing this stroke
+```
+
+The transformation matrix describes an affine transformation of each point in homogeneous coordinate system. Coordinates must be projected by this matrix before rendering.
+
+#### The points blob
+
+This describes the actual points of the drawing. Each point uses 24 bytes of data. The first 8 are the X and Y coordinates in *big endian* float32. I haven't tried to decode the rest, but it probably stores the pen pressure and maybe some acceleration information. 
+ 
---
+++ onyx_render.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+
+import tempfile
+from zipfile import ZipFile
+import sqlite3
+import os
+import json
+from fpdf import FPDF
+import numpy as np
+from tqdm import tqdm
+import sys
+
+def get_dir(dirs, parent):
+    dlist = []
+    while parent is not None:
+        p = dirs[parent]
+        parent = p["parent"]
+        dlist.insert(0, p["title"])
+
+    return os.path.join(*dlist) if dlist else ""
+
+def read_doc_list(tmpdir):
+    res = []
+
+    conn = sqlite3.connect(os.path.join(tmpdir, "ShapeDatabase.db"))
+    c = conn.cursor()
+    c.execute('select uniqueId,title,parentUniqueId from NoteModel where type = 0')
+    dirs = {}
+    for row in c:
+        id, title, parent = row
+        dirs[id] = {"title": title, "parent": parent}
+
+    c = conn.cursor()
+    c.execute('select uniqueId,title,pageNameList,parentUniqueId from NoteModel where type = 1')
+    for row in c:
+        id, title, namelist, parent = row
+        namelist = json.loads(namelist)["pageNameList"]
+
+        res.append({"id": id, "title": title, "pages": namelist,
+                    "dirname": get_dir(dirs, parent)})
+
+    return res
+
+def render_pdf(descriptor, tmpdir):
+    pdf = FPDF(orientation='P', unit='mm', format='letter')
+    scale = 279
+    width_scale = 0.1
+
+    conn = sqlite3.connect(os.path.join(tmpdir, descriptor["id"] + ".db"))
+
+    print("Rendering note %s" % descriptor["title"])
+    for page in tqdm(descriptor["pages"]):
+        pdf.add_page()
+
+        c = conn.cursor()
+        c.execute('select points, matrixValues, thickness from NewShapeModel where pageUniqueId = "'+page+'"')
+
+        for i, row in enumerate(c):
+            # Read / parse DB entries
+            points, matrix, thickness = row
+            matrix = np.asarray(json.loads(matrix)["values"], dtype=np.float32).reshape(3,3)
+
+            d = np.frombuffer(points, dtype=np.float32)
+            d = d.byteswap()
+
+            d = d.reshape(-1, 6)
+
+            # Projection matrix
+            points = d[:, :2]
+            points = np.concatenate((points, np.ones([points.shape[0],1])), axis=1)
+            points = points @ matrix.T
+            points = points[:, :2]
+
+            # Draw
+            pdf.set_line_width(thickness * width_scale)
+
+            x_off = 0
+            y_off = 0
+            for r in range(points.shape[0] - 1):
+                x_start = scale * (points[r][0] + x_off)
+                y_start = scale * (points[r][1] + y_off)
+
+                x_end = scale * (points[r + 1][0] + x_off)
+                y_end = scale * (points[r + 1][1] + y_off)
+
+                pdf.line(x_start, y_start, x_end, y_end)
+    return pdf
+
+def render_all(zip_name, save_to):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        # Extract note backup file
+        with ZipFile(zip_name, 'r') as zipObj:
+            zipObj.extractall(tmpdir)
+
+        notes = read_doc_list(tmpdir)
+        print("Found note structure:")
+        for note in notes:
+            print("   ", os.path.join(note["dirname"], note["title"]))
+
+        for note in notes:
+            pdf = render_pdf(note, tmpdir)
+            dir = os.path.join(save_to, note["dirname"])
+            os.makedirs(dir, exist_ok=True)
+
+            pdf.output(os.path.join(dir, "%s.pdf" % note["title"]))
+
+if __name__ == "__main__":
+    if len(sys.argv)!=3:
+        print("Usage: %s <note backup file> <dir to render>" % sys.argv[0])
+        sys.exit(-1)
+
+    zip_name = sys.argv[1]
+    save_to = sys.argv[2]
+
+    render_all(zip_name, save_to)
